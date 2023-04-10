72ca52687454446bb21191dd55d82eb3a9b497c5
Andriikryl <andreyk557@gmail.com>
2023-04-10 23:29:47 +0300

    feat[setA / setB function]

--- js/app.js
+++ js/app.js
@@ -1,4 +1,7 @@
 let CURRENT_LOCATION = null;
+let A = null;
+let B = null;
+
 function main() {
   let geolocation = null;
   if (window.navigator && window.navigator.geolocation) {
@@ -25,4 +28,31 @@ function onLocationUpdate(event) {
 
 function onError(err) {
   alert("cannot access location" + err);
+}
+
+function setA() {
+  A = CURRENT_LOCATION;
+  updateInfo();
+}
+
+function setB() {
+  B = CURRENT_LOCATION;
+  updateInfo();
+}
+
+function updateInfo() {
+  if (A != null) {
+    document.getElementById("aBtn").innerHTML =
+      A.latitude + "<br>" + A.longitude;
+  }
+
+  if (B != null) {
+    document.getElementById("bBtn").innerHTML =
+      B.latitude + "<br>" + B.longitude;
+  }
+
+  if (A != null && B != null) {
+    let dist = "?";
+    document.getElementById("info").innerHTML = "distance: " + dist + "meters";
+  }
 }
