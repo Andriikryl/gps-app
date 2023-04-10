0a0afe40cc37ad3d991b31a4b0ea24c747599b1b
Andriikryl <andreyk557@gmail.com>
2023-04-10 23:57:12 +0300

    feat[euclidean-funct]

--- js/app.js
+++ js/app.js
@@ -77,3 +77,11 @@ function getDistance(latlon1, latlon2) {
   const eucl = euclidean(xyz1, xyz2);
   return eucl;
 }
+
+function euclidean(p1, p2) {
+  return Math.sqrt(
+    (p1.x - p2.x) * (p1.x - p2.x) +
+      (p1.y - p2.y) * (p1.y - p2.y) +
+      (p1.z - p2.z) * (p1.z - p2.z)
+  );
+}
